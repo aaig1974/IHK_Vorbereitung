baa5983fe40a24c07e78dec2f22db9debec86f6f
aaig <aaig@DESKTOP-T7DAPN9>
2023-02-03 20:38:42 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using IHK_Vorbereitung.Models;
+
+namespace IHK_Vorbereitung.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<IHK_Vorbereitung.Models.Questions> Questions { get; set; }
+    }
+}
---
+++ Models/Questions.cs
@@ -0,0 +1,14 @@
+﻿namespace IHK_Vorbereitung.Models
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public Questions()
+        {
+
+        }
+    }
+}
